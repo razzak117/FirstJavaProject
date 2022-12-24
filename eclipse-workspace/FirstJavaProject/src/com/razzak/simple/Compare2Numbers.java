58b49c488c3559b6e03d27853ebf1480cc749097
razzak117 <razzak117@gmail.com>
2022-12-24 11:46:51 -0800

    Added a new class named Compare2Numbers

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/Compare2Numbers.java
@@ -0,0 +1,18 @@
+package com.razzak.simple;
+
+public class Compare2Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		int b;
+		
+		a=50;
+		b=30;
+		
+		if (a>b) System.out.println("First integer is bigger");
+		else System.out.println("Second integer is bigger");
+
+	}
+
+}
