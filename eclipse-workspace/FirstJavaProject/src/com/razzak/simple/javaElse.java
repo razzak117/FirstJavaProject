d1829834346bd4849b19d0d86b01ad74dc1f0326
razzak117 <razzak117@gmail.com>
2022-12-27 23:17:47 -0800

    first commit

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/javaElse.java
@@ -0,0 +1,23 @@
+package com.razzak.simple;
+
+public class javaElse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=60;
+		int b=70;
+		//int c=60;
+		
+		
+		if(a>b) { 
+			System.out.println("First int is bigger");
+		
+		}else { 
+			System.out.println("second int is bigger");
+		}
+	}
+
+}
+
+
