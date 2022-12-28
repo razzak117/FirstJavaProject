7deccf8b29a9d51b403420e2025e279b2caa5425
razzak117 <razzak117@gmail.com>
2022-12-28 04:20:40 -0800

    This is comit for forloop103

---
+++ eclipse-workspace/MyFirstJavaProject/src/com/myloop/forloop103.java
@@ -0,0 +1,16 @@
+package com.myloop;
+
+public class forloop103 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=120; i>20; i--) {
+			if(i%3==0) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
