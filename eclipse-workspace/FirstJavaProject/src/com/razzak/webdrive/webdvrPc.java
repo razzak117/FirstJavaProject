ea70a397a2e2f864dc7558a94f031ad5d5b393c6
razzak117 <razzak117@gmail.com>
2022-12-29 04:25:49 -0800

    Commit for webdriver

--- eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/webdvrPc.java
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/webdvrPc.java
@@ -4,6 +4,15 @@ public class webdvrPc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\SeleniumWD\\chrome\\v108\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://www.pcrichard.com/");
+		Thread.sleep(3000);
+		driver.manage().window().maximize();
+		Thread.sleep(2000);
+		driver.quit();
+	
 
 	}
 
