872a863942f3884dc81b915c1e3bae0969b2d6ff
razzak117 <razzak117@gmail.com>
2022-12-30 08:18:43 -0800

    commit ;ksajdpagdoh;'lk;k

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/ifelse104.java
@@ -0,0 +1,16 @@
+package com.razzak.simple;
+
+public class ifelse104 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=75;
+		int y=55;
+		
+		if (x>y) System.out.println("First integer is bigger");
+		else System.out.println("Second integer is bigger");
+
+
+	}
+
+}
