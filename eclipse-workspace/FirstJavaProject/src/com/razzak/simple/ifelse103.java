11619ad41ac53e46f6d349d4c1ac400621d23e79
razzak117 <razzak117@gmail.com>
2022-12-30 08:09:53 -0800

    commit for ifelse102

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/ifelse103.java
@@ -0,0 +1,16 @@
+package com.razzak.simple;
+
+public class ifelse103 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=90;
+		int b=160;
+		
+		if (a>b) System.out.println("First integer is bigger");
+		else System.out.println("Second integer is bigger");
+
+
+	}
+
+}
