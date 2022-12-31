a54e053591232a3ca1487430f0387c53df11ef4a
razzak117 <118125683+razzak117@users.noreply.github.com>
2022-12-31 13:17:04 -0500

    Noting important code added

--- eclipse-workspace/FirstJavaProject/src/com/razzak/simple/math_calc.java
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/math_calc.java
@@ -5,6 +5,8 @@ public class math_calc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("10 times 20: "+(10*20));
+		
+		//Nothing added but test
 	}
 
 }
