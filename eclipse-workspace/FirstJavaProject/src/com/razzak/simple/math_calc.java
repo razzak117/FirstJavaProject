1cbd9b3f52f51036fd10a16fbb30a2c644de70d7
razza <razza@DESKTOP-LOJ4G7F.lan>
2022-12-31 13:15:12 -0500

    math_calc library added

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/simple/math_calc.java
@@ -0,0 +1,10 @@
+package com.razzak.simple;
+
+public class math_calc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("10 times 20: "+(10*20));
+	}
+
+}
