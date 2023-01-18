050fa089ceba3836ca5c84312f7a8750dc1ad4d2
razzak117 <razzak117@gmail.com>
2023-01-18 05:16:54 -0800

    pkwhdoyewgdpiewjd'

--- eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/webdvrPc.java
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/webdvrPc.java
@@ -7,7 +7,7 @@ public class webdvrPc {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWD\\chrome\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumWD\\chrome\\v108\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		
 		driver.get("https://www.pcrichard.com/");
