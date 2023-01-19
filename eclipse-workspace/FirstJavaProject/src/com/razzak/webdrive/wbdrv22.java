0791844d6a8fbaaec5defc3a9be981d39cf3b750
razzak117 <razzak117@gmail.com>
2023-01-18 17:26:01 -0800

    Merge branch 'master' of https://github.com/razzak117/FirstJavaProject.git

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/wbdrv22.java
@@ -0,0 +1,21 @@
+package com.razzak.webdrive;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class wbdrv22 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\SeleniumWD\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().minimize();
+		Thread.sleep(3000);
+		driver.close();
+
+
+	}
+
+}
