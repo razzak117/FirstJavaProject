3e3f3f69f3a98f389ed2b469f1093cda162a82f3
razzak117 <razzak117@gmail.com>
2023-01-18 17:53:40 -0800

    commit for wbdrv23

---
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/wbdvr23.java
@@ -0,0 +1,21 @@
+package com.razzak.webdrive;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class wbdvr23 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\SeleniumWD\\chrome\\v108\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
+
+
+
+	}
+
+}
