659d5ab77cb11d1f1ba6d0c4e9f92a7ab195049e
razzak117 <razzak117@gmail.com>
2023-01-19 06:57:55 -0800

    commit for24

--- eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/wbdrv22.java
+++ eclipse-workspace/FirstJavaProject/src/com/razzak/webdrive/wbdrv22.java
@@ -8,13 +8,13 @@ public class wbdrv22 {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Lenovo\\Desktop\\SeleniumWD\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Lenovo\\Desktop\\SeleniumWD\\chrome\\v108\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://www.amazon.com/");
 		driver.manage().window().minimize();
 		Thread.sleep(3000);
 		driver.close();
-
+	
 
 	}
 
